a4efdbc9abcff46e23f0beefd14d2cb67a48f28e
Makson-B <maska.bas.dno@gmail.com>
2024-06-08 10:49:18 +0300

    Fix checking data for a duplicate

--- app/Console/Commands/FetchDataFromAPI.php
+++ app/Console/Commands/FetchDataFromAPI.php
@@ -50,6 +50,7 @@ class FetchDataFromAPI extends Command
         $sorted = $this->argument('sorted');
         $table = $this->argument('table');
 
+        //Проверка вызванной команды и вызов соответствующей функции "перелива" данных из API в DB
         if (!isset($sorted) && !isset($table)) {
             // Нет аргументов: загрузить все данные без сортировки
             $this->fetchAllData();
@@ -81,16 +82,18 @@ class FetchDataFromAPI extends Command
         $this->fetchData('stocks', $sorted);
     }
 
+    //получаем данные из API и вызываем функцию "перелива" их в БД
     protected function fetchData(string $dataType, bool $sorted = false): void
     {
+        //данные для запроса
         $token = config('api.api_key');
         $apiUrl = config('api.api_url');
         $dateFrom = $dataType === 'stocks' ? now()->format('Y-m-d') : '1900-01-01';
         $dateTo = now()->format('Y-m-d');
         $limit = '500';
         $page = 1;
-
         $endpoint = $this->endpoints[$dataType];
+
         $modelClass = $sorted ? $this->sortedModelMap[$dataType] : $this->modelMap[$dataType];
 
         $response = Http::get("{$apiUrl}{$endpoint}?dateFrom={$dateFrom}&dateTo={$dateTo}&page={$page}&key={$token}&limit={$limit}");
@@ -99,7 +102,7 @@ class FetchDataFromAPI extends Command
         $totalPages = $data['meta']['last_page'];
 
         $this->processPageData($data['data'], $modelClass, $sorted);
-        $this->info(ucfirst($dataType) . " page {$page} of {$totalPages} processed.");
+//        $this->info(ucfirst($dataType) . " page {$page} of {$totalPages} processed.");
 
         for ($page += 1; $page <= $totalPages; $page++) {
             $response = Http::get("{$apiUrl}{$endpoint}?dateFrom={$dateFrom}&dateTo={$dateTo}&page={$page}&key={$token}&limit={$limit}");
@@ -110,24 +113,28 @@ class FetchDataFromAPI extends Command
         $this->info(ucfirst($dataType) . " processed.");
     }
 
+    //Функция перелива данных в БД
     protected function processPageData(array $data, string $modelClass, bool $sorted): void
     {
         if ($sorted) {
+            //Вариант, если нужно сохранить отсортированные данные
             foreach ($data as $item) {
-                // Создаем новый экземпляр модели с указанными данными
-                $newModel = $modelClass::create($item);
-
-                // Проверяем, существует ли уже такая запись в базе данных
-                if (!$newModel->exists) {
-                    // Если записи нет, сохраняем её
-                    $newModel->save();
-                }
+                // Создаем новую строку в БД с указанными данными, если её нет
+                $modelClass::firstOrCreate($item);
             }
         } else {
-            // Если данные не сортированные, сохраняем их пачками json
+            //Вариант, если данные сохранять в json без сортировки
+            //Проверяем данные и удаляем уже имеющиеся в БД, чтобы не повторять
+            foreach ($data as $key => $data_item) {
+                $exists = $modelClass::whereRaw('JSON_CONTAINS(data, ?)', json_encode($data_item))->exists();
+                if ($exists) {
+                    unset($data[$key]);
+                }
+            }
+            // Сохраняем данные пачками json по 100, чтобы корректно помещались
             $dataChunks = array_chunk($data, 100);
             foreach ($dataChunks as $chunk) {
-                $modelClass::create(['data' => json_encode($chunk)]);
+                $modelClass::firstOrCreate(['data' => json_encode($chunk)]);
             }
             sleep(1);
         }

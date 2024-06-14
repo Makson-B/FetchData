a92a2a56983921ce66352b28c0588c81a150af11
Maksim-B <maska.bas.dno@gmail.com>
2024-06-14 18:15:52 +0300

    New update

--- config/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-    'api_url' => env('API_URL'),
-    'api_key' => env('API_KEY')
-];
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('fetch:data 1')
+    ->twiceDailyAt(8,20)
+    ->timezone('Europe/Moscow');
